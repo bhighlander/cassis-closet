1be121d4e12a7337049c0de710106a765850e07f
bhighlander <saphrimangel@gmail.com>
2023-10-20 13:43:57 -0500

    Added login and register pages

---
+++ src/components/views/ApplicationViews.js
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import { useNavigate, useLocation, Route, Routes } from "react-router-dom"
+import Login from "../auth/Login";
+import Register from "../auth/Register";
+
+export const ApplicationViews = ({ token, setToken }) => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        if (!token && location.pathname !== '/login' && location.pathname !== '/register') {
+            navigate("/login");
+        }
+    }, [token, navigate, location]);
+
+    if (!token) {
+        return (
+            <>
+            <Routes>
+                <Route>
+                    <Route path="/login" element={<Login setToken={setToken} />} />
+                    <Route path="/register" element={<Register setToken={setToken} />} />
+                </Route>
+            </Routes>
+            </>
+        )
+        
+    } else {
+    return (
+        <>
+        <Routes>
+            <Route>
+                <Route path="/login" element={<Login setToken={setToken} />} />
+                <Route path="/register" element={<Register setToken={setToken} />} />
+            </Route>
+        </Routes>
+        </>
+    )
+    }
+}
