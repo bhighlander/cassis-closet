0a63f83f7c0ea5be64b7aaf61864a012c8f211ef
bhighlander <saphrimangel@gmail.com>
2023-11-10 14:02:44 -0600

    Add getArticleById function and ArticleDetails component, update ArticleCard to include CardActionArea

--- src/components/clothingArticles/ArticleCard.js
+++ src/components/clothingArticles/ArticleCard.js
@@ -1,15 +1,23 @@
-import { Card, CardMedia } from "@mui/material"
+import { Card, CardActionArea, CardMedia } from "@mui/material"
+import { useNavigate } from "react-router-dom"
 
 export const ArticleCard = ({ article }) => {
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+        navigate(`/articles/${article.id}`)
+    }
 
     return (
         <Card className="articleCard">
+            <CardActionArea onClick={handleClick}>
             <CardMedia
             component="img"
             height="140"
             image={article.image}
             alt={article.type}
             />
+            </CardActionArea>
         </Card>
     )
 }
